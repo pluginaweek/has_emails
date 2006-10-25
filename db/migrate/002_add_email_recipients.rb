08b81c8497221acfb01a7cbdf58a5bf9922293cf
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2006-10-25 02:50:04 +0000

    Use strings for index names instead of symbols.

--- db/migrate/002_add_email_recipients.rb
+++ db/migrate/002_add_email_recipients.rb
@@ -2,13 +2,13 @@ class AddEmailRecipients < ActiveRecord::Migration
   def self.up
     add_column :message_recipients, :address_spec, :string, :limit => 384
     
-    remove_index :message_recipients, :name => :unique_message_recipients
-    add_index :message_recipients, [:message_id, :messageable_id, :type, :address_spec], :unique => true, :name => :unique_message_recipients
+    remove_index :message_recipients, :name => 'unique_message_recipients'
+    add_index :message_recipients, [:message_id, :messageable_id, :type, :address_spec], :unique => true, :name => 'unique_message_recipients'
   end
 
   def self.down
-    remove_index :message_recipients, :unique_message_recipients
-    add_index :message_recipients, [:message_id, :messageable_id, :type], :unique => true, :name => :unique_message_recipients
+    remove_index :message_recipients, :name => 'unique_message_recipients'
+    add_index :message_recipients, [:message_id, :messageable_id, :type], :unique => true, :name => 'unique_message_recipients'
     
     remove_column :message_recipients, :address_spec
   end
